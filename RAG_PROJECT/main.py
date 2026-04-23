a6fa607313e68ed06903fcb440e41e4a4aecf8ff
JANANE M U <mu.janane27@gmail.com>
2026-04-23 13:44:29 +0530

    Add files via upload

--- RAG_PROJECT/main.py
+++ RAG_PROJECT/main.py
@@ -1,1 +1,15 @@
+from src.rag_pipeline import build_pipeline, generate_answer
 
+def main():
+    db = build_pipeline("data/notes.pdf")
+
+    while True:
+        query = input("\nAsk: ")
+
+        if query == "exit":
+            break
+
+        print("\nAnswer:\n", generate_answer(db, query))
+
+if __name__ == "__main__":
+    main()
